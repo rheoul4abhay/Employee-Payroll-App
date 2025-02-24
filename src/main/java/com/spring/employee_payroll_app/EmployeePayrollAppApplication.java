b7eb17d1ba00a5807289b0072d1cd87a0f59cda8
Abhay Shrivastava <abhayshrivastava830@gmail.com>
2025-02-25 01:05:31 +0530

    [Abhay Shrivastava] Add . project setup

---
+++ src/main/java/com/spring/employee_payroll_app/EmployeePayrollAppApplication.java
@@ -0,0 +1,13 @@
+package com.spring.employee_payroll_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePayrollAppApplication.class, args);
+	}
+
+}
