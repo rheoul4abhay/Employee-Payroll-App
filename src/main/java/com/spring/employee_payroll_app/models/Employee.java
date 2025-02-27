081261c1758c7466f20d2ce4f7ba1a68f631ee0e
Abhay Shrivastava <abhayshrivastava830@gmail.com>
2025-02-27 15:53:00 +0530

    [Abhay Shrivastava] Add . custom logging levels

--- src/main/java/com/spring/employee_payroll_app/models/Employee.java
+++ src/main/java/com/spring/employee_payroll_app/models/Employee.java
@@ -1,11 +1,12 @@
 package com.spring.employee_payroll_app.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Table(name = "employee_payroll")
 public class Employee {
 
     @Id
@@ -17,34 +18,32 @@ public class Employee {
 
     public Employee(){};
 
-    public Employee(Long id, String name, double salary){
-        this.id = id;
+    public Employee(String name, double salary){
         this.name = name;
         this.salary = salary;
     }
 
-    //getters and setters
-    public long getId(){
-        return id;
-    }
-
-    public void setId(Long id){
-        this.id = id;
-    }
-
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public void setName(String name){
+    public void setName(String name) {
         this.name = name;
     }
 
-    public double getSalary(){
+    public double getSalary() {
         return salary;
     }
 
-    public void setSalary(double salary){
+    public void setSalary(double salary) {
         this.salary = salary;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 }
